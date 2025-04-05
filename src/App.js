5ba08e115fb437a8485fa588eebc34d412fb07f9
Terence Boakye <terenceboakye27@gmail.com>
2025-04-05 20:07:11 +0000

    tbm commited

---
+++ src/App.js
@@ -0,0 +1,21 @@
+//----------------------------------------
+// WHAT YOU SEE IN THE BROWSER
+//----------------------------------------
+import './App.css';
+import Header from './header';
+import Content from './content';
+import Footer from './footer'; 
+
+//the main content of the page
+function App() {
+  
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+)
+}
+
+export default App;
---
+++ src/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>
+      <p>
+        <h3>My Car List</h3>
+        </p>
+    </header>
+  )
+}
+
+export default Header;
